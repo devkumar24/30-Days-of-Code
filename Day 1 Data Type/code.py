ada2eb4fa4358d3903167f938beeef8352f32c41
DEV KUMAR <59363213+devkumar24@users.noreply.github.com>
2021-08-04 15:51:45 +0530

    added data type code

---
+++ Day 1 Data Type/code.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+int_var = int(input())
+double_var = float(input())
+str_var = input()
+# Read and save an integer, double, and String to your variables.
+int_var += i
+double_var += d
+s = s + str_var
+# Print the sum of both integer variables on a new line.
+print(int_var)
+# Print the sum of the double variables on a new line.
+print(double_var)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s)
